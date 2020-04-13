cef2b5cda454192b85f4f49a60527afb25afe58e
Willian Amaral <willian.amaral@outlook.com.br>
2020-04-12 22:09:05 -0300

    Updates

---
+++ precache-manifest.2001347334d770ca8ccda1b992836085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2d9a8ba8bfc4a50fadd4a1b8922e07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b99a96dbf2c527a150c",
+    "url": "/static/css/2.0e58bb66.chunk.css"
+  },
+  {
+    "revision": "0b99a96dbf2c527a150c",
+    "url": "/static/js/2.e1976469.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e1976469.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc8c0490a44e85dd79fa",
+    "url": "/static/js/main.9f3780fb.chunk.js"
+  },
+  {
+    "revision": "442a2cab505f62bfdb11",
+    "url": "/static/js/runtime-main.24a2a774.js"
+  },
+  {
+    "revision": "7eb13423ab2199a35a4672f72309afe4",
+    "url": "/static/media/bg-large.7eb13423.png"
+  }
+]);
